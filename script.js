e3606b8f4d651c7cdd957f474251d54ee608ce5d
Amit Giri <amitgiri257@gmail.com>
2023-08-26 22:30:31 +0530

    m2-f3

---
+++ script.js
@@ -0,0 +1,210 @@
+let menuInfo=[
+    {
+      "id": 1,
+      "name": "Cheeseburger",
+      "price": 5.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?cheeseburger"
+    },
+    {
+      "id": 2,
+      "name": "Pizza",
+      "price": 8.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?pizza"
+    },
+    {
+      "id": 3,
+      "name": "Tacos",
+      "price": 3.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?tacos"
+    },
+    {
+      "id": 4,
+      "name": "Sushi",
+      "price": 11.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?sushi"
+    },
+    {
+      "id": 5,
+      "name": "Pasta",
+      "price": 9.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?pasta"
+    },
+    {
+      "id": 6,
+      "name": "Fried Chicken",
+      "price": 7.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?fried_chicken"
+    },
+    {
+      "id": 7,
+      "name": "Grilled Cheese Sandwich",
+      "price": 4.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?grilled_cheese_sandwich"
+    },
+    {
+      "id": 8,
+      "name": "Steak",
+      "price": 15.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?steak"
+    },
+    {
+      "id": 9,
+      "name": "Caesar Salad",
+      "price": 6.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?caesar_salad"
+    },
+    {
+      "id": 10,
+      "name": "Fish and Chips",
+      "price": 8.49,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?fish_and_chips"
+    },
+    {
+      "id": 11,
+      "name": "Ramen",
+      "price": 9.49,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?ramen"
+    },
+    {
+      "id": 12,
+      "name": "Burrito",
+      "price": 7.49,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?burrito"
+    },
+    {
+      "id": 13,
+      "name": "Pho",
+      "price": 8.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?pho"
+    },
+    {
+      "id": 14,
+      "name": "Pad Thai",
+      "price": 9.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?pad_thai"
+    },
+    {
+      "id": 15,
+      "name": "Gyro",
+      "price": 6.49,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?gyro"
+    },
+    {
+      "id": 16,
+      "name": "Ice Cream",
+      "price": 3.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?ice_cream"
+    },
+    {
+      "id": 17,
+      "name": "Smoothie",
+      "price": 4.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?smoothie"
+    },
+    {
+      "id": "18",
+      "name": "Apple Pie",
+      "price": 4.49,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?apple_pie"
+    },
+    {
+      "id": 19,
+      "name": "Chocolate Cake",
+      "price": 5.49,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?chocolate_cake"
+    },
+    {
+      "id": 20,
+      "name": "Pancakes",
+      "price": 4.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?pancakes"
+    },
+    {
+      "id": 21,
+      "name": "Cupcake",
+      "price": 2.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?cupcake"
+    },
+    {
+      "id": 22,
+      "name": "Crepes",
+      "price": 5.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?crepes"
+    },
+    {
+      "id": 23,
+      "name": "Club Sandwich",
+      "price": 6.99,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?club_sandwich"
+    },
+    {
+      "id": 24,
+      "name": "Falafel",
+      "price": 5.49,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?falafel"
+    },
+    {
+      "id": 25,
+      "name": "Curry",
+      "price": 9.49,
+      "imgSrc": "https://source.unsplash.com/random/1920x1080/?curry"
+    }
+  ];
+  let json=JSON.stringify(menuInfo);
+async function getMenu() {
+    try {
+        const response = await fetch('json'); // Replace with your API endpoint
+        const menuItems = await response.json();
+        const menuDiv = document.getElementsByClassName("menu-list");
+        menuDiv.innerHTML = '<h2>Menu</h2>';
+        menuItems.forEach(item => {
+            menuDiv.innerHTML += `<p>${item.name} - $${item.price}</p>`;
+        });
+    } catch (error) {
+        console.error('Error fetching menu:', error);
+    }
+}
+
+function takeOrder() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const burgers = ['Burger A', 'Burger B', 'Burger C'];
+            const order = {
+                burgers: burgers[Math.floor(Math.random() * 3)],
+            };
+            resolve(order);
+        }, 2500);
+    });
+}
+
+function orderPrep(order) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve({ order_status: true, paid: false });
+        }, 1500);
+    });
+}
+
+function payOrder(order) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve({ order_status: true, paid: true });
+        }, 1000);
+    });
+}
+
+function thankyouFnc() {
+    alert('Thank you for eating with us today!');
+}
+
+async function placeOrder() {
+    await getMenu();
+    const order = await takeOrder();
+    const prepStatus = await orderPrep(order);
+    const paymentStatus = await payOrder(prepStatus);
+    if (paymentStatus.paid) {
+        thankyouFnc();
+    }
+}
+
+document.getElementById('menu').addEventListener('click', placeOrder);
